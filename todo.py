37e519e0c9672ef6a1aa40e78240c76a06d583a1
SUYOG_K <suyogkudachi21@gmail.com>
2024-08-08 14:54:45 +0530

    First Task

--- todo.py
+++ todo.py
@@ -32,22 +32,22 @@ def deltask():
         if delchoice != 'Y':
             break
 
-def updttask():
+def modtask():
     while True:
         if not tasks:
-            print("No tasks to update.")
+            print("No tasks to modify.")
             break
-        print("Please select the task to be updated (by number): ")
+        print("Please select the task to modified (by number): ")
         for i, task in enumerate(tasks, 1):
             print(f"{i}. {task}")
         m = int(input()) - 1
         if 0 <= m < len(tasks):
-            print("Please enter the updated task: ")
+            print("Please enter the modified task: ")
             tasks[m] = input("")
-            print("\nTask updated.")
+            print("\nTask modified.")
         else:
             print("Invalid input.")
-        print("Do you want to update another task? (Y/N): ")
+        print("Do you want to modify another task? (Y/N): ")
         updtchoice = input("").upper()
         if updtchoice != 'Y':
             break
@@ -66,7 +66,7 @@ if __name__ == "__main__":
         print("++++++++++++++++++++++++++++++++++++++++++++++++")
         print("\n1. Add a new task")
         print("2. Delete a task")
-        print("3. Update a task")
+        print("3. Modify a task")
         print("4. View all tasks")
         print("5. Exit")
         print("\nPlease select one of the above options:")
@@ -77,11 +77,11 @@ if __name__ == "__main__":
         elif user == 2:
             deltask()
         elif user == 3:
-            updttask()
+            modtask()
         elif user == 4:
             view_tasks()
         elif user == 5:
-            print("Exiting the program. Goodbye!")
+            print("Exiting the app. Goodbye!")
             break
         else:
             print("Invalid input, please try again.")
